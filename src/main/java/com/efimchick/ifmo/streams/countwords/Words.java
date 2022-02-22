ca8997dbc7d361a0a75de496016269e3328d8818
AnnaTsukiyo <annett.sav4enkko@gmail.com>
2022-02-22 23:50:16 +0200

    done

--- src/main/java/com/efimchick/ifmo/streams/countwords/Words.java
+++ src/main/java/com/efimchick/ifmo/streams/countwords/Words.java
@@ -1,12 +1,35 @@
 package com.efimchick.ifmo.streams.countwords;
 
-
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Words {
+    private static final String REGEX = "\\P{javaLetter}+";
+    private static final int MIN_LENGTH_WORD = 4;
+    private static final int MIN_AMOUNT_WORDS = 10;
 
     public String countWords(List<String> lines) {
+        String string = lines.stream()
+                .flatMap(line -> Arrays.stream(line.split(REGEX))
+                        .map(String::toLowerCase))
+                .filter(word -> word.length() >= MIN_LENGTH_WORD)
+                .collect(Collectors.groupingBy(word -> word, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .filter(collectorListEntry -> collectorListEntry.getValue() >= MIN_AMOUNT_WORDS)
+                .sorted(Map.Entry.comparingByKey())
+                .sorted((o1, o2) -> o2.getValue().compareTo(o1.getValue()))
+                .map(stringLongEntry -> String.format("%s - %d\n", stringLongEntry.getKey(), stringLongEntry.getValue()))
+                .collect(Collectors.joining());
+        StringBuilder stringBuilder = removeLastChar(string);
+        return stringBuilder.toString();
+    }
 
-        return null;
+    private StringBuilder removeLastChar(String string) {
+        StringBuilder stringBuilder = new StringBuilder(string);
+        stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+        return stringBuilder;
     }
 }
